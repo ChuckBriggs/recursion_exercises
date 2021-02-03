3626ef0c6319b62d385e6b93d134e8f4bcd46977
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2021-02-03 01:39:35 -0700

    condensed some of the logic

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,21 +3,16 @@
 
 def merge_sort(arr)
   if arr.length < 2
-    sorted = arr
+    arr
   else
     sorted = []
     left = merge_sort(arr[0..arr.length / 2 - 1])
     right = merge_sort(arr[arr.length / 2..-1])
     while left[0] && right[0]
-      if left[0] > right[0]
-        sorted << right.shift
-      else
-        sorted << left.shift
-      end
+      sorted << (left[0] > right[0] ? right.shift : left.shift)
     end
     sorted += left + right
   end
-  sorted
 end
 
 arr = [1, 6, 4, 3, 7, 4, 0, 8]
